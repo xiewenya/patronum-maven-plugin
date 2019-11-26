ebf2442aa202dfc1219fca3761dcc24f1c60536a
xuewenyao <bresai@live.cn>
2019-11-26 15:34:47 +0800

    init

---
+++ core/src/main/java/com/bresai/expecto/patronum/core/PatronumException.java
@@ -0,0 +1,22 @@
+package com.bresai.expecto.patronum.core;
+
+/**
+ * @version 1.0
+ * @author:bresai
+ * @date:2019/11/26
+ * @content:
+ */
+public class PatronumException extends Exception {
+    private static final long serialVersionUID = 3769339967824377319L;
+
+    public PatronumException() {
+    }
+
+    public PatronumException(String message) {
+        super(message);
+    }
+
+    public PatronumException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
